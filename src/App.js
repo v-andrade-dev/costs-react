63e93e777697e71307549e678a18bf306760c07c
v-andrade-dev <vini.andrade0@gmail.com>
2023-03-13 17:42:52 -0300

    implementado componentes navbar e footer

--- src/App.js
+++ src/App.js
@@ -1,31 +1,30 @@
-import {BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import Home from './components/pages/Home';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
-import Container from './components/layout/Container';
+import Container from './components/layout/container/Container';
+import Navbar from './components/layout/navbar/Navbar';
+import Footer from './components/layout/footer/Footer';
+import Projects from './components/pages/projects/Projects';
 
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='company'>Company</Link>
-        <Link to='contact'>Contato</Link>
-        <Link to='newproject'>Novo Projeto</Link>
-      </div>
+      <Navbar />
 
       <Container customClass='min-height'>
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/projects' element={<Projects />} /> 
           <Route exact path='/company' element={<Company />} />
           <Route exact path='/contact' element={<Contact />} />
           <Route exact path='/newproject' element={<NewProject />} /> 
-          </Routes>
+        </Routes>
       </Container>
-
-      <p>Footer</p>
+      <Footer />
     </Router>
 
   );
---
+++ src/components/layout/navbar/Navbar.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+import Container from "../container/Container";
+import styles from "./Navbar.module.css";
+import logo from "../../../img/costs_logo.png";
+
+function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+            <Container>
+                <Link to='/'>
+                    <img src={logo} alt="Simbolo costs"></img>
+                </Link>
+                <ul className={styles.list}>
+                    <li className={styles.item}><Link to='/'>Home</Link></li>
+                    <li className={styles.item}><Link to='/projects'>Projetos</Link></li>
+                    <li className={styles.item}><Link to='company'>Quem Somos</Link></li>
+                    <li className={styles.item}><Link to='contact'>Contato</Link></li>
+                </ul>
+            </Container>
+        </nav>
+    )
+}
+
+export default Navbar;
