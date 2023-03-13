d4f537625143852ac5cd951e5349b50ec10917ff
v-andrade-dev <vini.andrade0@gmail.com>
2023-03-13 16:54:06 -0300

    criação do projeto base, dependencias, limpeza, estrutura e componente container

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Container from './components/layout/Container';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='company'>Company</Link>
+        <Link to='contact'>Contato</Link>
+        <Link to='newproject'>Novo Projeto</Link>
+      </div>
+
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} /> 
+          </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </Router>
+
   );
 }
 
