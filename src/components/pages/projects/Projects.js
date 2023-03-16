7f30fba669be2cda6718988b00b09a34f17f5c61
v-andrade-dev <vini.andrade0@gmail.com>
2023-03-16 11:04:45 -0300

    configuração de dashboard basico dos comonente projects

--- src/components/pages/projects/Projects.js
+++ src/components/pages/projects/Projects.js
@@ -3,7 +3,8 @@ import { useLocation } from 'react-router-dom';
 import styles from './Projects.module.css';
 
 import Message from '../../layout/message/Message';
-
+import LinkButton from '../../layout/linkButton/LinkButton';
+import Container from '../../layout/container/Container';
 
 function Projects(){
 
@@ -14,11 +15,19 @@ function Projects(){
     }
 
     return (
-        <div>
-            <h1>Meus Projetos</h1>
-            {
-                message && <Message type="success" msg={message} />
-            }
+        <div className={styles.project_container}>
+            <div className={styles.title_container}>
+                <h1>Projetos Cadastrados</h1>
+                <LinkButton to='/newproject' text='Criar Projeto'></LinkButton>
+            </div>
+            <div>
+                {message && <Message type="success" msg={message} />}
+                <Container customClass="start">
+                    <p>Projetos</p>
+                </Container>
+            </div>
+            
+            
             
         </div>
     )
